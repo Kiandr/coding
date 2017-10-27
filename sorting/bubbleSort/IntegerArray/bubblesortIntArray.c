18412fbc210d9ce84d95d77be5ed13b05b82d5b4
Kian Davoudi-Rad <davoudi.rk@gmail.com>
2017-10-27 08:08:24 -0700

    adding new stuff

---
+++ sorting/bubbleSort/IntegerArray/bubblesortIntArray.c
@@ -0,0 +1,18 @@
+void bubbleSortIntArray(){
+
+
+
+}
+
+int main(){
+
+int arr[]={1,2,3,4,5,6};
+
+
+for (int i=0; i< sizeof(arr)/sizeof(int);i++){
+printf("i= %d i[%d]=%d \n", i,i,arr[i]);
+
+}
+
+return 0;
+}
