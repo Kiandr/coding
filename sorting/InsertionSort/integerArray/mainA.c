1deb59ae0001c8acdbfe648a519b4698fdad9896
Kian Davoudi-Rad <davoudi.rk@gmail.com>
2017-11-02 10:12:11 -0700

    adding the main array

---
+++ sorting/InsertionSort/integerArray/mainA.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+void sortArray( int const *head, int len){
+
+
+
+}
+
+
+int main(){
+
+int array []={1,2,3,4,5}
+
+int len = sizeof(array)/sizeof(int);
+
+sortArray (array, len);
+
+return 0;
+}
