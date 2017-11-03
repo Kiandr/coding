dc04b5b9d2ea114ceb79a1e4ecb144546d31a896
Kian Davoudi-Rad <davoudi.rk@gmail.com>
2017-11-03 09:26:57 -0700

    adding only one file

--- sorting/InsertionSort/integerArray/mainA.c
+++ sorting/InsertionSort/integerArray/mainA.c
@@ -1,15 +1,31 @@
 #include "stdio.h"
 
-void sortArray( int const *head, int len){
+void sortArray( int const *header, int len){
 
 
+	int *prtH = NULL;
+
+	// this is a temp value that changes based on the iteration, its the right memebr of the first member of unsorted array
+	int key=0;
+
+	for (int i=0;i<len;i++){
+		{
+			// setup pointers for each iteration
+			prtH = header[i];
+			key = header[i+1];
+			printf("key = %d, prtH=%d", key, *prtH)l;
+
+
+		}
+		
+	}
 
 }
 
 
 int main(){
 
-int array []={1,2,3,4,5}
+int array []={7,8,5,2,4,6,3}
 
 int len = sizeof(array)/sizeof(int);
 
