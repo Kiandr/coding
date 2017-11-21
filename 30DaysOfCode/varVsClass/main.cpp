99b28aa40b68d24daf6f0330d375ff529d8d3b69
Kian Davoudi-Rad <kiandavoudi-rad@Kians-MacBook-Pro.local>
2017-11-21 10:43:51 -0800

    adding changes

--- 30DaysOfCode/varVsClass/main.cpp
+++ 30DaysOfCode/varVsClass/main.cpp
@@ -1,25 +1,40 @@
+// Headers./	
 #include <iostream>
-
 using namespace std;
-
+// Person Class
 class Person{
-Pulic:
-int age; 
-void amOld();
-void yearPasses();
-};
-
-// Implementation
-Person::Person(int initialAge){
-
-}
-void Person:amOld(){
-
-}
-
-void Person:yeasPasses(){
-}
-
+    public:
+        int age;
+        Person(int initialAge);
+        void amIOld();
+        void yearPasses();
+    };
+	// Person Implementation 
+    Person::Person(int initialAge){
+        // Add some more code to run some checks on initialAge
+		if (initialAge>=0)
+			this->age = initialAge;
+		else {
+			this->age = 0;
+		std::cout<<"Age is not valid, setting age to 0."<<std::endl;
+		}
+    }
+	// amOld Implementation 
+    void Person::amIOld(){
+        // Do some computations in here and print out the correct statement to the console
+		if (this->age<13)
+			std::cout<<"You are young."<<std::endl;
+		else if (this->age<18 || this->age>=13)
+			std::cout<<"You are teenager."<<std::endl;
+		else 
+			std::cout<<"You are old."<<std::endl;
+    }
+	// yearpass implementation 
+    void Person::yearPasses(){
+        // Increment the age of the person in here
+		this->age++;
+    }
+	// Main code
 int main(){
     int t;
 	int age;
@@ -32,8 +47,7 @@ int main(){
         	p.yearPasses(); 
         }
         p.amIOld();
-      
 		cout << '\n';
     }
-
     return 0;
+}
