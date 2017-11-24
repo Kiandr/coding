2fceb11e238476e7661b8b5048ab62c27bde2dbd
Kian Davoudi-Rad <kiandavoudi-rad@Kians-MacBook-Pro.local>
2017-11-24 09:31:32 -0800

    finally the question wanted the odd and even indexes not the letters. So stupid

--- 30DaysOfCode/review/main.c
+++ 30DaysOfCode/review/main.c
@@ -1,21 +1,72 @@
-#include <iostream>
-using namespace std;
+#include "stdio.h"
+#include "string.h"
+
+void foo(char *head, int len)
+{
+	// debugging
+	//	printf("[%d\n",&, len);
+	char *prtH = head; 
+	int lenEv =0;
+	int lenOd =0;
+	{
+		while (prtH<=(head+len) && *prtH != '\n')
+		{
+			if (*prtH%2 == 0)
+				lenEv++;
+			else 
+				lenOd++;
+			prtH++;
+		}
+//		printf("sizeof odd = %d\n", lenOd);
+//		printf("sizeof even = %d\n", lenEv);
+		char arrayEv[lenEv];
+		char arrayOd[lenOd];
+		
+		char *prtArrayEv  = arrayEv;
+		char *prtAarrayOd = arrayOd;
+		
+		prtH = head;
+
+		while (prtH<=(head+len) && *prtH != '\n')
+		{
+			if (*prtH%2 == 0 &&*prtH!=0){
+//				printf("Even letter is %d -> %c\n",*prtH, *prtH);
+				*prtArrayEv++ = *prtH++;
+
+			}
+			else {
+//				printf("Odd letter is %d -> %c\n",*prtH, *prtH);
+				*prtAarrayOd++ = *prtH++;
+			}
+		}
+//		printf("START\n");		
+		for (int i=0;i<lenEv;i++)
+			printf("%c",arrayEv[i]);
+		printf(" ");
+		for (int i=0;i<lenOd;i++)
+			printf("%c",arrayOd[i]);
+		printf("\n");		
+
+		
+	}
+	
+	
+}
 
 int main() {
-    int x;
-    cin >> x;
-    for(int i = 0; i < x; i++){
-        string s;
-        string s1, s2;
-        cin >> s;
-        for(int j=0; j < s.size(); j++){
-            if(j%2==0){
-                s1 += s[j];
-            }else{
-                s2+= s[j];
-            }
-        }
-        cout << s1 << " " << s2 << endl;
-    }
-    return 0;
+	int loop=0;
+	char array[100];
+
+	scanf("%d",&loop);
+	for (int i=0;i<loop;i++){
+		scanf("%s", array);
+		int len = strlen (array);
+		char newArray [len];
+		strcpy (newArray, array);
+//		printf("[%lu->%s]\n", strlen(newArray), newArray);
+		foo(newArray, sizeof(newArray)/sizeof(char));
+	}
+	
+
+return 0;
 }
