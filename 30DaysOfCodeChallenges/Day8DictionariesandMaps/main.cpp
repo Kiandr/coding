d60a8f45c09e5b7fb71628959f9ea00e2d3c959e
Kian Davoudi-Rad <kiandavoudi-rad@Kians-MacBook-Pro.local>
2017-12-05 10:04:59 -0800

    clean the code for sharing

--- 30DaysOfCodeChallenges/Day8DictionariesandMaps/main.cpp
+++ 30DaysOfCodeChallenges/Day8DictionariesandMaps/main.cpp
@@ -21,19 +21,16 @@ int main() {
 
 		for (int i=0;i<loop;i++)
 		{
-	//	cout<<"Key"<<endl;
 		cin>>key;
 		cin.ignore();
-	//	cout<<"val"<<endl;
 		cin>>val;
 		cin.ignore();
 		phoneBook[key] = val;
-//			cout<<"Key="<<key<<"| Val="<<val<<"AND"<< phoneBook[key] <<endl;
 		}
 		 
 		 
 		 
-		   string name; 
+		string name; 
 		while(cin >> name) {
 		    if (pBook.find(name) != pBook.end()) {
 		        cout << name << "=" << pBook.find(name)->second << endl;
@@ -41,24 +38,6 @@ int main() {
 		        cout << "Not found" << endl;
 		    }
 		}
-		 
-		 
-		 
-		/* 
-		 
-		  string name; 
-//		  cout<<"Enter a name"<<endl;
-		for (int i=0;i<loop;i++){
-			cin>>name;
-			cin.ignore();
-			
-				    if (phoneBook.find(name) != phoneBook.end()) {
-				           cout << name << "=" << phoneBook.find(name)->second << endl;
-				       } else {
-				           cout << "Not found" << endl;
-				       }
-				   }
-		*/
 				   
     return 0;
 }
