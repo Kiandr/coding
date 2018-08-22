16580ccd11e4088bd47c49197107399427114aa3
Kian Davoudi-Rad <kiandr@Kians-MBP.localdomain>
2018-08-22 13:45:49 -0700

    SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* head, int data, int position)

--- Practice/linkedListMacCompiler/linkedListMacCompiler/main.cpp
+++ Practice/linkedListMacCompiler/linkedListMacCompiler/main.cpp
@@ -111,8 +111,31 @@ struct Node *reverseList(struct Node *head){
     }
     
     return newHead;
+}
+
+
+struct Node* insertNodeAtPosition(struct Node* head, int data, int position) {
+    
+    if (head == NULL) return head;
+    struct Node *prt = head;
+    int i = 1;
+    while (prt!=NULL){
+        if (i==position){
+            struct Node *temp =  (struct Node*)malloc(sizeof(struct Node));
+            temp->val = data ;
+            temp->next = prt->next;
+            prt->next = temp;
+            return head;
+            
+        }
+        i++;
+        prt=prt->next;
+        
+    }
+    return head;
     
 }
+
 
 int main() {
     struct Node *head = NULL;
@@ -120,12 +143,12 @@ int main() {
     head = Add(head, 20);
     head = Add(head, 30);
     head = Add(head, 40);
-    
-    print(head);
+    insertNodeAtPosition(head, 25,2);
+    //print(head);
     //reverse(head);
     //reverse_stack(head);
     //reverseVector(head);
-    head = reverseList(head);
+    //head = reverseList(head);
     print(head);
     std::cout << "End of progarm";
     return 0;
