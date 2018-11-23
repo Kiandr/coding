186a9cdbc631db23a1420c6d29642bdfc2b26fe9
Kian D. Rad <davoudi.rk@gmail.com>
2018-11-23 11:45:46 -0800

    adding fixed array length for c++ implementation

---
+++ Algorithm/DividableBySum/DotNet/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+namespace DotNet
+{
+    public class Facad<T>where T:new(){
+
+        public Facad(){}
+        public T  Pairs(int [] array, int sum ){
+            T myDic = new T();
+            Console.WriteLine("Hello Controller");
+            Console.WriteLine(myDic.GetType());
+            return (T) myDic;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] myArray ={2,4,8,10};
+            var getPair = new Facad<Dictionary<int,int>>().Pairs(myArray, 2);
+
+            Console.WriteLine("End of main function ");
+        }
+    }
+}
--- PNIDigital/findPairOfSum.cpp
+++ PNIDigital/findPairOfSum.cpp
@@ -2,15 +2,15 @@
 #include <map>
 #include <iterator>
 
-static std::map <int, int> findPairsOfSum(int array[], int sum){
+static std::map <int, int> findPairsOfSum(int array[], int sum, int size){
     std::map <int, int> myPair;
-    int len = sizeof(array);
+    int len = size;
     if (array == NULL) return myPair;
     for (int i=0; i<len; i++)
         for (int j = i+1; j<len; j++)
         {
             if (array[i]+array[j] == sum)
-            myPair.insert(std::pair<int,int>(array[i], array[j]));
+                myPair.insert(std::pair<int,int>(array[i], array[j]));
         }
     return myPair;
 }
@@ -18,12 +18,13 @@ static std::map <int, int> findPairsOfSum(int array[], int sum){
 
 int main(){
     std::map <int, int> pairs;
-    int myArray[] = {0,10,3,7,3,5,7,3};
-    pairs = findPairsOfSum(myArray,10);
+    int myArray[] = {0,10,3,7,3,5,7,3,5,5,4,6,2,8,1,9,10,10,10,0,8,2};
+    //std::cout<<sizeof(myArray)/sizeof(*myArray)<<'\n'; //--> debugging purposes
+    pairs = findPairsOfSum(myArray,10, sizeof(myArray)/sizeof(*myArray));
     // printing map
     std::map <int, int> :: iterator it;
     for (it = pairs.begin(); it != pairs.end(); ++it){
-      std::cout<<it->first<<' '<<it->second <<'\n';
+        std::cout<<it->first<<' '<<it->second <<'\n';
     }
     return 0;
 }
