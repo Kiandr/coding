cc3f7b09b11e5e6b5974d98c2f0ddb676d5fe02f
Kian <kian@Kians-iMac-Pro.lan>
2018-11-28 08:44:59 -0800

    pairs question

--- Practice/Search/Pairs.cpp
+++ Practice/Search/Pairs.cpp
@@ -1,75 +1,73 @@
-#include <bits/stdc++.h>
+#include<vector>
+#include <iostream>
+#include <any>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
 
-using namespace std;
+bool isInVector(std::vector<std::pair<int, int>> myVector, int first, int second){
+    for(auto it = myVector.begin(); it!=myVector.end(); it++ ){
+        if(it->first == first && it->second == second){
+            return 1;
+        }
 
-vector<string> split_string(string);
+}
+            return 0;
+}
+
 
 // Complete the pairs function below.
-int pairs(int k, vector<int> arr) {
-
-
+std::vector<std::pair<int,int>>  pairs(int k, std::vector<int> arr) {
+    std::vector<std::pair<int,int>> newVec;
+    newVec.push_back(std::pair<int,int>(0,0));
+    
+    for (int i=0;i<arr.size();i++)
+        for(int j=i+1;j<arr.size();j++)
+            if (arr.at(i)+arr.at(j) == k){
+                if (isInVector(newVec,arr.at(i), arr.at(j))!=1 ){
+                std::cout<<arr.at(i)<< "---"<<arr.at(j) <<"Here is -->"<<arr.at(i)+arr.at(j) <<std::endl;
+                    newVec.push_back(std::pair<int,int>(arr.at(i), arr.at(j)));}
+            }
+    
+    return  newVec;
 }
 
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string nk_temp;
-    getline(cin, nk_temp);
-
-    vector<string> nk = split_string(nk_temp);
-
-    int n = stoi(nk[0]);
-
-    int k = stoi(nk[1]);
-
-    string arr_temp_temp;
-    getline(cin, arr_temp_temp);
-
-    vector<string> arr_temp = split_string(arr_temp_temp);
-
-    vector<int> arr(n);
-
-    for (int i = 0; i < n; i++) {
-        int arr_item = stoi(arr_temp[i]);
-
-        arr[i] = arr_item;
-    }
-
-    int result = pairs(k, arr);
-
-    fout << result << "\n";
-
-    fout.close();
+    
+    std::vector<int> myvector;
+    std::vector<int> test;
+    std::vector<int>::iterator it = myvector.begin();
+    
+    // Adding randome values into the vector
+    for (int i=0; i <100; i++)
+//        myvector.push_back(rand() % 100);
+        myvector.push_back(i);
+    for (int i=0; i <100; i++)
+        //        myvector.push_back(rand() % 100);
+        myvector.push_back(i);
+    
+//    // Reading Vector with at operator
+//    for (int i=0;i<myvector.size();i++)
+//        std::cout<<myvector.at(i)<<std::endl;
+//
+//    // Transvers using the iterator
+//    for (it = myvector.begin(); it !=myvector.end();++it)
+//        std::cout<<*it<<std::endl;
+//
+//
+//    for (it = myvector.begin(); it !=myvector.end();++it)
+//        std::cout<<*it<<std::endl;
+    
+    
+    std::vector<std::pair<int,int>> newVec;
+    newVec = pairs(20, myvector);
+    for(int i = 0; i < newVec.size(); i++)
+        std::cout << newVec.at(i).first << ", " << newVec.at(i).second <<std::endl;
 
     return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
 }
