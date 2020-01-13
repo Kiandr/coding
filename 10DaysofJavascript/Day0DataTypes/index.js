d47b3ba157c926141d4439c796a90568fa02f88d
kian <kian.davoudirad@ddswireless.com>
2020-01-13 13:00:19 -0800

    adding parsing

--- 10DaysofJavascript/Day0DataTypes/index.js
+++ 10DaysofJavascript/Day0DataTypes/index.js
@@ -12,10 +12,8 @@ function performOperation(secondInteger, secondDecimal, secondString) {
     console.log(firstInteger+ parseInt(secondInteger));
 
     // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
-    console.log(Number.parseFloat(firstDecimal) + Number.parseFloat(secondDecimal));
+    console.log(firstDecimal + Number(secondDecimal));
 
     // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
-    console.log(`${firstString}${secondString}`);
+    console.log(`${firstString}${secondString}`)
 }
-
-
