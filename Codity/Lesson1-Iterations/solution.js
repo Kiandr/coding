7365463944815a6c70875e68d574b2628b5c1197
kian <kian.davoudirad@ddswireless.com>
2020-06-15 07:57:20 -0700

    Update

--- Codity/Lesson1-Iterations/solution.js
+++ Codity/Lesson1-Iterations/solution.js
@@ -2,48 +2,37 @@
 
 class solution {
     constructor(data) {
-        this.returnValue = '10';
-        // this.inputData = data.split('\n');
-        this.inputData = [1, 3, 6, 4, 1, 2];
+        this.returnValue = 0;
+        this.inputData = data.split('\n');
+        // this.inputData = [1, 3, 6, 4, 1, 2];
     }
 
     get() {
         let res = [];
         // res.push(this.execute(this.inputData[1].split(' ')));
-        res.push(this.execute(this.inputData));
+        res.push(this.execute(parseInt(this.inputData[1])));
         this.returnValue = res;
         return this.returnValue;
     }
 
-    execute(arr) {
+    execute(N) {
         // write your code in JavaScript (Node.js 8.9.4)
-        let a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-        let sortedArray = [];
-        let len = arr.length;
-        for (var i = 0; i < len; i++) {
-            for (var j = 0; j < len - i - 1; j++) { // this was missing
-                if (arr[j] > arr[j + 1]) {
-                    // swap
-                    var temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-        for (let i = 0; i < a.length - 1; i++) {
-            if (!sortedArray.includes(arr[i]))
-                sortedArray.push(arr[i]);
-            arr[i] = '';
-        }
-        sortedArray.sort();
-        for (let i = 0; i < sortedArray.length; i++) {
-            if (sortedArray[i] !== a[i]) {
-                return parseInt(a[i]);
+        let arr = N.toString(2).split('')
+        // console.log( arr)
+        let a = [];
+        for (let i = 0; i <arr.length ; i++) {
+            if (parseInt(arr[i]) === 1){
+                // console.log(i);
+                a.push(i);
             }
 
-        }
 
-        return 0;
+        }
+        // console.log(a)
+        let a0 = a[0] === 0 ? 1 : parseInt(a[0]);
+        let a1 = parseInt(a[1]);
+        return a1 - a0 > 0 ? a1 - a0 : 0;
+
     }
 
 }
