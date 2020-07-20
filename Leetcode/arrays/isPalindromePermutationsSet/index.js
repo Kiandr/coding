e3d25a7601303101ecb985b1de4052b32ffa2efd
kian <kian.davoudirad@ddswireless.com>
2020-07-20 10:50:07 -0700

    used associative array and a map for counting.

--- Leetcode/arrays/isPalindromePermutationsSet/index.js
+++ Leetcode/arrays/isPalindromePermutationsSet/index.js
@@ -2,16 +2,24 @@ const isPalindromePermutation = (s) => {
     if (s.length <= 1) return true;
     let myMap = new Map();
     for (let i = 0; i < s.length; i++) {
-        myMap.set(s[i], myMap.has(s[i]) ? myMap.get(s[i]) + 1 : 0);
+        myMap.set(s[i], myMap.has(s[i]) ? myMap.get(s[i]) + 1 : 1);
     }
 
     let myArray = [];
     for (let i = 0; i < s.length; i++) {
-        myArray[s.charAt(i)] = myArray[s.charAt(i)] === undefined ? 0 : myArray[s.charAt(i)] + 1;
+        myArray[s.charAt(i)] = myArray[s.charAt(i)] === undefined ? 1 : myArray[s.charAt(i)] + 1;
     }
+
+
+    let res =  myMap.forEach((value, key, map) => {
+    console.log(`${key} ,${value} ${value%2}`);
 
     console.log(myMap);
     console.log(myArray);
+            return value%2;
+        });
+
+    return res;
 };
 
 console.log(isPalindromePermutation('rracecarr') === true);
