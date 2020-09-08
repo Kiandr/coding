90795293403e6f7eac67588a5289eb00b6007cd3
kian <info@kian.co>
2020-09-08 06:56:45 -0700

    update

--- practice/TwoSum/index.js
+++ practice/TwoSum/index.js
@@ -11,17 +11,26 @@
  return [0, 1].
 
  */
-function foo(nums, target){
+function foo(nums, target) {
   let myArray = [];
-  console.log(Array.isArray(nums));
-  if (!Array.isArray(nums)||nums.length===0||parseInt(target)<0) return 0;
-  for (let i = 0; i < nums.length-1; i++) {
-    if(nums[i]+nums[i+1] === parseInt(target)){
-      myArray.push(i)
-      myArray.push(i+1)
+  if (!Array.isArray(nums) || nums.length === 0 || parseInt(target) < 0) return 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if(nums[i]+nums[j]===target) {
+      if(!myArray.includes(i))myArray.push(i);
+        // console.log(target);
+      }
+     // console.log(nums[i], nums[j],nums[i]+ nums[j]);
     }
-  }
-  return myArray;
+
+  } return myArray
 }
 
-console.log(foo([2, 7, 11, 15], 9));
+// let res = foo([2, 7, 11, 15], 9);
+// res.forEach((value, index, array)=>{
+//   console.log([0, 1][index]===value)
+// })
+// console.log( foo([1, 2, 3], 4))
+console.log( foo([2, 7, 11, 15], 9))
+// 0 -2
