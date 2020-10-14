72f12acb1153794be25a933ee328af156fa2b565
kian <kian.davoudirad@ddswireless.com>
2020-10-14 08:12:38 -0700

    ankurDb

---
+++ practice/rotateString/index.js
@@ -0,0 +1,14 @@
+'use strict';
+/**
+We are given two strings, A and B.
+A shift on A consists of taking string A and moving the leftmost character to the rightmost position.
+For example, if A = 'abcde', then it will be 'bcdea' after one shift on A. Return True if and only
+if A can become B after some number of shifts on A.
+*/
+
+function foo(A, B){
+
+}
+
+console.log(foo('abcde','cdeab')===true)
+console.log(foo('abcde','abced')===false)
