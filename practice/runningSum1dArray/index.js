d86086fe8f57fcd1905065a582ff54bfb9c6daad
kian <kian.davoudirad@ddswireless.com>
2020-10-20 07:58:00 -0700

    Runtime: 84 ms, faster than 36.73% of JavaScript online submissions for Running Sum of 1d Array.

---
+++ practice/runningSum1dArray/index.js
@@ -0,0 +1,53 @@
+/***
+ * https://leetcode.com/problems/running-sum-of-1d-array/
+ Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+ Return the running sum of nums.
+ Example 1:
+ Input: nums = [1,2,3,4]
+ Output: [1,3,6,10]
+ Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+ Example 2:
+ Input: nums = [1,1,1,1,1]
+ Output: [1,2,3,4,5]
+ Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+ Example 3:
+ Input: nums = [3,1,2,10,1]
+ Output: [3,4,6,16,17]
+
+ Constraints:
+
+ 1 <= nums.length <= 1000
+ -10^6 <= nums[i] <= 10^6
+
+ */
+
+
+function A(nums) {
+  return nums.reduce((a, b) => a + b);
+}
+
+let runningSum = function(nums) {
+  let sum=0;
+  for(let i=0;i<nums.length;i++){
+    sum=sum+nums[i];
+    nums[i]=sum;
+  }
+  return nums;
+};
+
+function foo(nums) {
+  if (!Array.isArray(nums) || nums.length <= 0 || false || nums === 0 || nums === null) return nums;
+  let sumArray = [];
+  let sum = nums[0];
+  sumArray.push(sum);
+  for (let i = 1; i < nums.length; i++) {
+    sum = sum + parseInt(nums[i]);
+    sumArray.push(sum);
+  }
+  return sumArray;
+
+}
+
+console.log(foo([1, 2, 3, 4]));
