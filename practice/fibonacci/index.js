f26ee8e05b7eaeb3b3dedc973edae2729cb289e0
kian <info@kian.co>
2020-10-20 19:59:16 -0700

    update

--- practice/fibonacci/index.js
+++ practice/fibonacci/index.js
@@ -1,6 +1,7 @@
 /**
- * Given two binary strings, return their sum (also a binary string).
+ * https://leetcode.com/problems/add-binary/submissions/
 
+ * Given two binary strings, return their sum (also a binary string).
  The input strings are both non-empty and contains only characters 1 or 0.
 
  Example 1:
