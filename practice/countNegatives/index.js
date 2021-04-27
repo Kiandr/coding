5d1dd9db26938299b7a1db65da617d1305ef5293
kian <info@kian.co>
2021-04-27 09:49:54 -0700

    countNegatives

--- practice/countNegatives/index.js
+++ practice/countNegatives/index.js
@@ -2,11 +2,37 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var countNegatives = function(grid) {
-    
+var countNegatives = function (grid) {
+    let count = 0;
+
+    for (let i = 0; i < grid.length; i++) {
+
+        for (let j = 0; j <= grid[i].length; ) {
+            count = grid[i][j] < 0 ? ++count : count
+            j++;
+        }
+    }
+    return count;
 };
+// console.log(countNegatives([[4, 3, 2, -1], [3, 2, 1, -1], [1, 1, -1, -2], [-1, -1, -2, -3]]));
+// console.log(countNegatives([[5, 1, 0], [-5, -5, -5]]));
+console.log(countNegatives([[6, 5, 4, 4, 3, -2, -2, -2], [5, -2, -2, -3, -3, -4, -4, -4], [-3, -3, -3, -4, -4, -5, -5, -5], [-3, -5, -5, -5, -5, -5, -5, -5], [-4, -5, -5, -5, -5, -5, -5, -5]]));
+// console.log(countNegatives([
+//     [0,1,2,3,4,5],
+//     [1,1,2,3,4,5],
+//     [2,1,2,3,4,5],
+//     [3,1,2,3,4,5],
+//     [4,1,2,3,4,5],
+//     [5,1,2,3,4,5]
+// ]));
 /*
+https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/discuss/1179337/runtime-90.82-of-javascript-andand-memory-usage-61.43-of-javascript
+Runtime: 76 ms, faster than 90.37% of JavaScript online submissions for Count Negative Numbers in a Sorted Matrix.
+Memory Usage: 40 MB, less than 60.42% of JavaScript online submissions for Count Negative Numbers in a Sorted Matrix.
+
+
 https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+
 Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
 
  
