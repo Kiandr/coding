3e32cdaae5297fa1a332cc6b2800f4ac8aa0200d
kian <info@kian.co>
2021-06-15 11:36:55 -0700

    flowered

---
+++ codity/array/canPlaceFlowers/index.js
@@ -0,0 +1,67 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+
+    let counter = 0;
+
+    for (let i = 0; i < flowerbed.length; i++) {
+
+        if (flowerbed[i] === 0 && (i === 0 || flowerbed[i - 1] === 0) && (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)) {
+            flowerbed[i] = 1;
+            counter++;
+        }
+    }
+
+    return counter >= n;
+    // return count
+};
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 1)) // 1
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 2))
+console.log(canPlaceFlowers([1, 0, 0, 0, 0, 0, 1], 2))
+console.log(canPlaceFlowers([0, 0, 1, 0, 1], 1))
+console.log(canPlaceFlowers([1, 0, 0, 0, 1, 0, 0], 2))
+console.log(canPlaceFlowers([1, 0], 1))
+console.log(canPlaceFlowers([0], 1))
+console.log(canPlaceFlowers([0, 1, 0], 1))
+console.log(canPlaceFlowers([1, 0, 0, 0, 1, 0, 0], 2))
+// console.log(canPlaceFlowers([1, 0, 0, 0, 1, 0, 0], 2))
+
+// console.log(canPlaceFlowers([1, 2, 3, 4, 5, 6, 7], 2))
+/*
+* https://leetcode.com/problems/can-place-flowers/
+* 605. Can Place Flowers
+Easy
+
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
+
+
+
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+
+Example 2:
+
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+
+
+
+Constraints:
+
+    1 <= flowerbed.length <= 2 * 104
+    flowerbed[i] is 0 or 1.
+    There are no two adjacent flowers in flowerbed.
+    0 <= n <= flowerbed.length
+
+Accepted
+178.4K
+Submissions
+564.2K
+* */
