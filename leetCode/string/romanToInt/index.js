4a43570fdc794e092e1de7c5fd7bdd9a2754a88f
kian <info@kian.co>
2021-07-07 10:18:47 -0700

    romans to integer

--- leetCode/string/romanToInt/index.js
+++ leetCode/string/romanToInt/index.js
@@ -30,7 +30,28 @@ console.log(romanToInt("MCMXCIV")) // 1994
 console.log(romanToInt("LVIII")) // 58
 console.log(romanToInt("IX")) // 9
 console.log(romanToInt("IV")) // 4
+var romanToInt02 = function(s) {
+    result = 0
 
+    let roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        let current = s[i]
+        let next = s[i + 1]
+
+        roman[current] < roman[next] ? result  -= roman[current] : result += roman[current]
+    }
+
+    return result
+}
 /*
 https://leetcode.com/problems/roman-to-integer/
 * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
